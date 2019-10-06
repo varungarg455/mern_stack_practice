77bf877dc8b4f38921c9e2d0d41ec917c1886ab3
Varun Garg <varungarg455@gmail.com>
2019-10-06 23:07:47 +0530

    Setting up routing

--- mern-todo-app/src/App.js
+++ mern-todo-app/src/App.js
@@ -6,11 +6,30 @@ import TodosList from './components/todos-list.component';
 import EditTodo from './components/edit-todo.component';
 import CreateTodo from './components/create-todo.component';
 
+import logo from './logo.svg';
+
 function App() {
   return (
     <Router>
       <div className="container">
-        <h2>MERN Stack TODO App</h2> 
+
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <a href="http://www.google.com" className="navbar-brand" target="_blank">
+            <img src={logo} width="30" height="30" alt="wesite logo"/>
+          </a>
+          <Link to="/" className="navbar-brand">MERN Stack Todo App</Link>
+          <div className="collapse navbar-collapse">
+            <ul className="navbar-nav mr-auto">
+              <li className="navbar-item">
+                <Link to="/" className="nav-link">Todos</Link>
+              </li>
+              <li className="navbar-item">
+                <Link to="/create" className="nav-link">Create Todo</Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+
         <Route path="/" exact component={TodosList} />
         <Route path="/edit/:id" component={EditTodo} />
         <Route path="/create" component={CreateTodo} />
