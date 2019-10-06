8f0483c0ed77201b1a563d651c014b748cdd0baf
Varun Garg <varun.garg@publicissapient.com>
2019-10-06 21:36:02 +0530

    Commit changes

--- mern-todo-app/src/App.js
+++ mern-todo-app/src/App.js
@@ -2,15 +2,19 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import TodosList from './components/todos-list.component';
+import EditTodo from './components/edit-todo.component';
+import CreateTodo from './components/create-todo.component';
+
 function App() {
   return (
     <Router>
       <div className="container">
-        <h2>MERN Stack TODO App</h2>
+        <h2>MERN Stack TODO App</h2> 
+        <Route path="/" exact component={TodosList} />
+        <Route path="/edit/:id" component={EditTodo} />
+        <Route path="/create" component={CreateTodo} />
       </div>
-      <Route path="/" exact component={TodosList} />
-      <Route path="/edit/:id" component={EditTodo} />
-      <Route path="/create" component={CreateTodo} />
     </Router>
   );
 }
