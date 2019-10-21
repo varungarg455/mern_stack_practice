71147a83fe5a0a747fed5535f03299fbb4619371
varungarg455 <varungarg455@gmail.com>
2019-10-21 17:44:34 +0530

    todo routes implementation

--- mern-todo-app/backend/server.js
+++ mern-todo-app/backend/server.js
@@ -20,6 +20,56 @@ connection.once('open', function(){
     console.log('MongoDB database connection established successfully');
 });
 
+todoRoutes.route('/').get(function(req, res){
+    Todo.find(function(err, todos){
+        if(err){
+            console.log(err);
+        } else{
+            res.json(todos);
+        }
+    });
+});
+
+todoRoutes.route('/:id').get(function(req, res){
+    let id = req.params.id;
+    Todo.findById(id, function(err, todo){
+        res.json(todo);
+    });
+});
+
+todoRoutes.route('/add').post(function(req, res){
+    let todo = new Todo(req.body);
+    todo.save()
+        .then(function(todo){
+            res.status(200).json({'todo': 'todo added successfully'});
+        })
+        .catch(function(err){
+            res.status(400).send('adding new todo failed');
+        });
+});
+
+todoRoutes.route('/update/:id').post(function(req, res){
+    Todo.findById(req.params.id, function(err, todo){
+        if(!todo){
+            res.status(404).send('data not found');
+        } else {
+            todo.todo_description = req.body.todo_description;
+            todo.todo_responsible = req.body.todo_responsible;
+            todo.todo_priority = req.body.todo_priority;
+            todo.todo_completed = req.body.todo_completed;
+
+            todo.save().then(todo => {
+                res.status(200).json('Todo updated');
+            })
+            .catch(err => {
+                res.status(400).send("Update not possible");
+            });
+        }
+    });
+});
+
+app.use('/todos', todoRoutes);
+
 app.listen(PORT, function(){
     console.log(`Server is running on PORT : ${PORT}`);
 });
