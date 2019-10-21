016cad2447255b7459054a0756e050e4189df3df
varungarg455 <varungarg455@gmail.com>
2019-10-21 15:10:03 +0530

    Connection to mongodb database

--- mern-todo-app/backend/server.js
+++ mern-todo-app/backend/server.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
 const app = express();
 const PORT = 4000;
 
 app.use(cors());
 app.use(bodyParser.json);
+
+//Connection to mongo db database
+mongoose.connect('mongodb://admin:Inspiron1525@ds331758.mlab.com:31758/todos', { useNewUrlParser: true });
+const connection = mongoose.connection;
+
+connection.once('open', function(){
+    console.log('MongoDB database connection established successfully');
+    
+});
 
 app.listen(PORT, function(){
     console.log(`Server is running on PORT : ${PORT}`);
