177f432bf6e489e1b4747c462cad9b63684ae90b
varungarg455 <varungarg455@gmail.com>
2020-02-10 13:15:02 +0530

    some commits

---
+++ rabbitMQ/Example6/consumer.js
@@ -0,0 +1,45 @@
+var amqp = require('amqplib/callback_api');
+
+var url = "amqp://mjeayvar:oQ2qwcyqZp2fLc50inxalbYtgdp_stg-@vulture.rmq.cloudamqp.com/mjeayvar";
+
+var args = process.argv.slice(2);
+
+if (args.length == 0) {
+    console.log("Usage: receive_logs_direct.js [info] [warning] [error]");
+    process.exit(1);
+}
+
+amqp.connect(url, function (err_conn, conn) {
+    if (err_conn) {
+        throw err_conn;
+    }
+    conn.createChannel(function (err_channel, channel) {
+        if (err_channel) {
+            throw err_channel;
+        }
+        var exchange = 'amq.topic';
+
+        channel.assertExchange(exchange, 'topic', {
+            durable: true
+        });
+
+        channel.assertQueue('', {
+            exclusive: true
+        }, function (err_queue, q) {
+            if (err_queue) {
+                throw err_queue;
+            }
+            console.log(`Waiting for messages in queue: ${q.queue}`);
+
+            args.forEach(function (key) {
+                channel.bindQueue(q.queue, exchange, key);
+            });
+
+            channel.consume(q.queue, function (msg) {
+                console.log(" [x] %s: '%s'", msg.fields.routingKey, msg.content.toString());
+            }, {
+                noAck: true
+            });
+        });
+    });
+});
