4b546d7ff333b2d87c55579f9bcd124cb81b8062
Varun Garg <varun.garg@publicissapient.com>
2020-03-29 19:14:38 +0530

    changes

---
+++ advanced-css-project/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+B
+Natours/index.html,2/6/2684c95b5eb56c0c4b56e5c3e339c6d8f7881f61
+E
+Natours/css/style.css,8/7/8791d40e2fa5fb39a1d4871d4948da8989be42cc
---
+++ redux-example/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+import {Provider} from 'react-redux';
+
+import Posts from './components/Posts';
+import Postform from './components/Postform';
+import store from './store';
+
+function App() {
+    return (
+        <Provider store={store}>
+            <div className="App">
+                <Postform/>
+                <hr/>
+                <Posts/>
+            </div>
+        </Provider>
+    );
+}
+
+export default App;
